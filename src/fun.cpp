d458cca98bbcfe1e5c8ae08ec5edd46115b61135
A-Sabu <a.saburova04@mail.ru>
2025-03-10 11:52:26 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,5 +2,7 @@
 #include "fun.h"
 
 int64_t power(int64_t x, uint16_t n) {
-    return 0;
+    if (n == 0) return 1;
+    else if (n % 2 == 0) return power(x * x, n / 2);
+    else return x * power(x * x, (n - 1) / 2);
 }
